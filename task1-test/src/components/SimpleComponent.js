d3fb86a691e810ed32a5e888146691b9c12a4bb9
panasik <viktar_panasik@epam.com>
2021-02-14 21:37:40 +0300

    delete

--- task1-test/src/components/SimpleComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class SimpleComponent extends React.Component {
-    render(){
-        return <div>Hello world from React.Component.</div>
-    }
-}
-
-export default SimpleComponent;
